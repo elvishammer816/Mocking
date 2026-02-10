3525aacc28d496cabb8978d44e9b3943b49dd5e9
elvishammer816 <108972904+elvishammer816@users.noreply.github.com>
2026-02-10 14:32:35 +0530

    Update testbook_scraper.py

--- testbook_scraper.py
+++ testbook_scraper.py
@@ -15,7 +15,10 @@ class TestBookScraper:
         chrome_options.add_argument('--headless')
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-dev-shm-usage')
-        
+
+        # Tell Selenium where Chromium is installed
+        chrome_options.binary_location = "/usr/bin/chromium"
+
         self.driver = webdriver.Chrome(
             service=Service(ChromeDriverManager().install()),
             options=chrome_options
